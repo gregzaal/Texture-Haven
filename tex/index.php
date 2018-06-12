c2c35fbc15763d5e33a4426e3977a08dedbb5524
Greg <gregzzmail@gmail.com>
2018-06-12 21:09:42 +0200

    temp fix for broken map list

--- tex/index.php
+++ tex/index.php
@@ -79,8 +79,10 @@ if ($is_published){
                 if ($f != $all_maps_f){
                     $without_ext = pathinfo($f, PATHINFO_FILENAME);
                     $ext = strtolower(pathinfo($f, PATHINFO_EXTENSION));
-                    $map_type = substr($without_ext, strlen($slug)+1, strlen($r)*-1-1);
-                    $downloads[$map_type][$r][$ext] = $f;
+                    if ($ext != 'zip'){
+                        $map_type = substr($without_ext, strlen($slug)+1, strlen($r)*-1-1);
+                        $downloads[$map_type][$r][$ext] = $f;
+                    }
                 }
             }
         }
