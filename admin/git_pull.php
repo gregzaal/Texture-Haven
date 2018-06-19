a929745dd2a81e5f29bf94f0cb8627c97166ece6
Greg <gregzzmail@gmail.com>
2018-06-19 10:55:18 +0200

    Add git pull page

---
+++ admin/git_pull.php
@@ -0,0 +1,37 @@
+<?php
+include ($_SERVER['DOCUMENT_ROOT'].'/php/functions.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Git Pull</title>
+    <link href='/css/style.css' rel='stylesheet' type='text/css' />
+    <link href='/css/admin.css' rel='stylesheet' type='text/css' />
+    <link href="https://fonts.googleapis.com/css?family=PT+Mono" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+</head>
+<body>
+
+<?php include ($_SERVER['DOCUMENT_ROOT'].'/admin/header.php'); ?>
+<div id="page-wrapper">
+<div id="page">
+
+<h1>Update Website from Git</h1>
+
+<?php 
+
+$cmd = "pwd && ls";
+
+$output = shell_exec($cmd);
+
+echo $output;
+
+?>
+
+</div>
+</div>
+
+</body>
+</html>
