cc1621e262c61bef4b4fdd2db94a367ce6831aae
gregzaal <gregzzmail@gmail.com>
2019-10-04 16:43:25 +0200

    Fix json list page

--- php/json_list.php
+++ php/json_list.php
@@ -6,8 +6,8 @@ include($_SERVER['DOCUMENT_ROOT'].'/php/html/cache_top.php');
 $textures = get_from_db();
 $json = array();
 foreach ($textures as $t){
-    $t = $t['tags'].";".$t['categories'];
-    $json[$t['slug']] = $t;
+    $tags = $t['tags'].";".$t['categories'];
+    $json[$t['slug']] = $tags;
 }
 
 echo json_encode($json, JSON_PRETTY_PRINT);
