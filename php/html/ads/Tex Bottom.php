1df286d52d834f7156bd0cbe9bc011ba75705129
Greg Zaal <gregzzmail@gmail.com>
2021-02-17 10:26:34 +0200

    Wrong ad code

--- php/html/ads/Tex Bottom.php
+++ php/html/ads/Tex Bottom.php
@@ -8,7 +8,7 @@
 <ins class="adsbygoogle ad-tex-bottom"
      data-ad-client="ca-pub-2284751191864068"
      <?php if($GLOBALS['WORKING_LOCALLY']){echo "data-adtest=\"on\"";} ?>
-     data-ad-slot="6138177566"></ins>
+     data-ad-slot="6441429897"></ins>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
