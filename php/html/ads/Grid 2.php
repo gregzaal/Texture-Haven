212cedccd17eb1bc6044783258446cab48cde633
Greg Zaal <gregzzmail@gmail.com>
2021-02-17 10:42:26 +0200

    Use unique ad codes from HDRI Haven for analytics

--- php/html/ads/Grid 2.php
+++ php/html/ads/Grid 2.php
@@ -4,7 +4,7 @@
      style="display:inline-block;width:336px;height:280px"
      data-ad-client="ca-pub-2284751191864068"
      <?php if($GLOBALS['WORKING_LOCALLY']){echo "data-adtest=\"on\"";} ?>
-     data-ad-slot="3910347000"></ins>
+     data-ad-slot="3788200994"></ins>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
--- php/html/ads/Sidebar.php
+++ php/html/ads/Sidebar.php
@@ -4,7 +4,7 @@
      style="display:inline-block;width:200px;height:200px"
      data-ad-client="ca-pub-2284751191864068"
      <?php if($GLOBALS['WORKING_LOCALLY']){echo "data-adtest=\"on\"";} ?>
-     data-ad-slot="4181153207"></ins>
+     data-ad-slot="9214775737"></ins>
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
